e2f15aac2ce6a521614e485ab2633cf3c7a7e1c4
Christian Abila <christian.abila@yahoo.com>
2014-07-26 09:03:03 +0200

    Update duplicates.cpp

--- duplicates.cpp
+++ duplicates.cpp
@@ -3,17 +3,15 @@
 int main()
 {
 	int a[] = {3, 1, 1, 3};
-	int beingCompared;
 	
 	for(int i = 0; i < 3; i++) //O(n)
 	{
-		beingCompared = a[i]; //O(n)
 		
 		for(int j = i+1; j < 4; j++) //O(n)
 		{
-			if(beingCompared == a[j]) //O(n)
+			if(a[i] == a[j]) //O(n)
 			{
-				printf("%d\n", beingCompared);
+				printf("%d\n", a[i]);
 				break;
 			}
 		}
